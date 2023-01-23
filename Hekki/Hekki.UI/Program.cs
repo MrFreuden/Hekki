def2a3ca74408c730e7d9802dea88a27bf631709
MrFreuden <mrfreuden@gmail.com>
2023-01-23 11:37:13 +0200

    test publish

--- Hekki/Hekki.UI/Program.cs
+++ Hekki/Hekki.UI/Program.cs
@@ -1,3 +1,5 @@
+using Squirrel;
+
 namespace Hekki.Hekki.UI
 {
     internal static class Program
@@ -8,10 +10,29 @@ namespace Hekki.Hekki.UI
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: OnAppInstall,
+                onAppUninstall: OnAppUninstall,
+                onEveryRun: OnAppRun);
             ApplicationConfiguration.Initialize();
             Application.Run(new Main());
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
         }
     }
 }
