2801466ad885f895fe7e128fb8dcb56cef26cdcf
MrFreuden <mrfreuden@gmail.com>
2024-08-05 11:49:55 +0200

    Add tests projects

---
+++ ExcelControllerTests/Services/ExcelReaderTest.cs
@@ -0,0 +1,16 @@
+namespace ExcelControllerTests.Services
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+ tr
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
