1754e2a90ba1b40c1dbadd8661adfc0c4bced7c5
MrFreuden <mrfreuden@gmail.com>
2024-08-18 01:46:03 +0200

    1. **Added a new `using` directive:** 2. **Uncommented and modified the `GetCellsByValue_ReturnsCorrectRanges` test 3. **Uncommented and modified the `FindNext_ReturnsCorrectChain` test method:**

--- ExcelControllerTests/Services/ExcelSearcherTest.cs
+++ ExcelControllerTests/Services/ExcelSearcherTest.cs
@@ -2,6 +2,7 @@ using Moq;
 using ExcelController.Services;
 using ExcelController.Services.InteropWrappers;
 using ExcelControllerTests.Moq;
+using System.Reflection.PortableExecutable;
 namespace ExcelControllerTests.Services
 {
     [TestFixture]
@@ -19,55 +20,63 @@ namespace ExcelControllerTests.Services
             _mockFactory = new ExcelRangeMockFactory();
         }
 
-        //[Test]
-        //public void GetCellsByValue_ReturnsCorrectRanges()
-        //{
-        //    // Arrange
-        //    var searchValue = "Test Value";
-        //    var row = 1;
-        //    var col = 1;
-        //    var count = 2;
-        //    var headers = _mockFactory.CreateHeaders(row, col, searchValue, count, count + count);
-        //    _mockFactory.SetupSearchRange(_mockSearchedRange, headers);
-        //    var excelSearcher = new ExcelSearcher(_mockExcel.Object);
+        [Test]
+        public void GetCellsByValue_ReturnsCorrectRanges()
+        {
+            // Arrange
+            var searchValue = "Test Value";
+            var row = 1;
+            var col = 1;
+            var count = 2;
+            var headers = new List<Mock<IExcelRange>>();
+            for (int i = 0; i < count; i++)
+            {
+                headers.Add(_mockFactory.CreateHeader(row, col + i, searchValue));
+            }
+            _mockFactory.SetupSearchRange(_mockSearchedRange, headers);
+            var excelSearcher = new ExcelSearcher(_mockExcel.Object);
 
-        //    // Act
-        //    var result = excelSearcher.GetCellsByValue(searchValue, _mockSearchedRange.Object);
+            // Act
+            var result = excelSearcher.GetCellsByValue(searchValue, _mockSearchedRange.Object);
 
-        //    // Assert
-        //    Assert.That(result, Has.Count.EqualTo(count));
-        //    Assert.That(result[0].Address, Is.EqualTo("$A$1"));
-        //    Assert.That(result[1].Address, Is.EqualTo("$E$1"));
-        //}
+            // Assert
+            Assert.That(result, Has.Count.EqualTo(count));
+            Assert.That(result[0].Address, Is.EqualTo("$A$1"));
+            Assert.That(result[1].Address, Is.EqualTo("$B$1"));
+        }
 
-        //[Test]
-        //public void FindNext_ReturnsCorrectChain()
-        //{
-        //    // Arrange
-        //    var searchValue = "Test Value";
-        //    var row = 1;
-        //    var col = 1;
-        //    var count = 3;
-        //    var headers = _mockFactory.CreateHeaders(row, col, searchValue, count, count + count);
-        //    _mockFactory.SetupSearchRange(_mockSearchedRange, headers);
+        [Test]
+        public void FindNext_ReturnsCorrectChain()
+        {
+            // Arrange
+            var searchValue = "Test Value";
+            var row = 1;
+            var col = 1;
+            var count = 3;
+            var headers = new List<Mock<IExcelRange>>();
+            for (int i = 0; i < count; i++)
+            {
+                headers.Add(_mockFactory.CreateHeader(row, col + i, searchValue));
+            }
+            _mockFactory.SetupSearchRange(_mockSearchedRange, headers);
 
-        //    // Act
-        //    var firstRange = _mockSearchedRange.Object.Find("Test Value");
-        //    var secondRange = _mockSearchedRange.Object.FindNext(firstRange);
-        //    var thirdRange = _mockSearchedRange.Object.FindNext(secondRange);
-        //    var fourthRange = _mockSearchedRange.Object.FindNext(thirdRange);
+            // Act
+            var firstRange = _mockSearchedRange.Object.Find("Test Value");
+            var secondRange = _mockSearchedRange.Object.FindNext(firstRange);
+            var thirdRange = _mockSearchedRange.Object.FindNext(secondRange);
+            var fourthRange = _mockSearchedRange.Object.FindNext(thirdRange);
 
-        //    // Assert
-        //    Assert.That(firstRange, Is.Not.Null);
-        //    Assert.That(firstRange.Address, Is.EqualTo("$A$1"));
+            // Assert
+            Assert.That(firstRange, Is.Not.Null);
+            Assert.That(firstRange.Address, Is.EqualTo("$A$1"));
 
-        //    Assert.That(secondRange, Is.Not.Null);
-        //    Assert.That(secondRange.Address, Is.EqualTo("$G$1"));
+            Assert.That(secondRange, Is.Not.Null);
+            Assert.That(secondRange.Address, Is.EqualTo("$B$1"));
 
-        //    Assert.That(thirdRange, Is.Not.Null);
-        //    Assert.That(thirdRange.Address, Is.EqualTo("$M$1"));
+            Assert.That(thirdRange, Is.Not.Null);
+            Assert.That(thirdRange.Address, Is.EqualTo("$C$1"));
 
-        //    Assert.That(fourthRange.Address, Is.EqualTo(fourthRange.Address));
-        //}
+            Assert.That(fourthRange.Address, Is.EqualTo(fourthRange.Address));
+        }
     }
 }
