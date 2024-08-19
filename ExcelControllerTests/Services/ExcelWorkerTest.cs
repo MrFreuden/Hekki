abb74a13419fcd79546d18a221c8e53dc8a0a5e4
MrFreuden <mrfreuden@gmail.com>
2024-08-19 02:48:53 +0200

    Code: add new Read tests

--- ExcelControllerTests/Services/ExcelWorkerTest.cs
+++ ExcelControllerTests/Services/ExcelWorkerTest.cs
@@ -222,6 +222,66 @@ namespace ExcelControllerTests.Services
             AssertResults(result, expected);
         }
 
+        [Test]
+        public void ReadDataInColumnsByName_ReturnsEmptyList_WhenColumnNamesDoesNotExist()
+        {
+            // Arrange
+            var columnName = "Time";
+            var countData = 3;
+            var offset = 1;
+            var columnIndex = _fixture.Create<int>();
+            var columnOfData = _mockFactory.CreateColumnWithData(
+                ExcelWorker.StartRowByDefault - 1, columnIndex + offset, columnName, countData, Moq.ValueType.RandomNumber);
+            var expected = new List<List<string>>();
+            _mockSearcher.Setup(x => x.GetCellsByValue("Пілоти", null)).Returns(new List<IExcelRange>());
+            SetupColumns(columnOfData);
+
+            // Act
+            var result = _excelWorker.ReadDataInColumnsByName(columnName, countData);
+
+            // Assert
+            AssertResults(result, expected);
+        }
+
+        [Test]
+        public void ReadDataInColumnsByName_ReturnsEmptyList_WhenColumnDoesNotExist()
+        {
+            // Arrange
+            var columnName = "Time";
+            var countData = 3;
+            var expected = new List<List<string>>();
+            _mockSearcher.Setup(x => x.GetCellsByValue(It.IsAny<string>(), null)).Returns(new List<IExcelRange>());
+
+            // Act
+            var result = _excelWorker.ReadDataInColumnsByName(columnName, countData);
+
+            // Assert
+            AssertResults(result, expected);
+        }
+
+        [Test]
+        public void ReadDataInColumnsByName_ReturnsExpected_WhenCountMoreThanCountData()
+        {
+            // Arrange
+            var columnName = "Time";
+            var countData = 3;
+            var offset = 1;
+            var columnIndex = _fixture.Create<int>();
+            var columnOfNames = _mockFactory.CreateColumnWithData(
+                ExcelWorker.StartRowByDefault - 1, columnIndex, "Пілоти", countData, Moq.ValueType.RandomString);
+            var columnOfData = _mockFactory.CreateColumnWithData(
+                ExcelWorker.StartRowByDefault - 1, columnIndex + offset, columnName, countData, Moq.ValueType.RandomNumber);
+            var expected = CreateExpectedData(columnOfData);
+            SetupColumns(columnOfNames);
+            SetupColumns(columnOfData);
+
+            // Act
+            var result = _excelWorker.ReadDataInColumnsByName(columnName, countData + 1);
+
+            // Assert
+            AssertResults(result, expected);
+        }
+
         private List<List<string>> CreateExpectedData(List<IExcelRange> columnData)
         {
             return new List<List<string>> { columnData.Skip(1).Select(cell => cell.Value2.ToString()).ToList() };
@@ -252,6 +312,22 @@ namespace ExcelControllerTests.Services
 
             // Assert
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1, 0)]
+        [TestCase(0, 1)]
+        [TestCase(0, 0)]
+        public void ReadDataInColumn_ReturnsException_WhenIndexesIsLessThenOne(int startRow, int columnIndex)
+        {
+            // Arrange
+            int count = 3;
+            var cells = _mockFactory.CreateCellsInColumn(startRow, columnIndex, count).Select(x => x.Object).ToList();
+            var expected = cells.Select(cell => cell.Value2.ToString()).ToList();
+            SetupReader(new List<List<IExcelRange>> { cells });
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _excelWorker.ReadDataInColumn(startRow, columnIndex, count));
         }
     }
 }
