76736b8404ba7cd4275ddc9f78783cbcd709ef5a
MrFreuden <mrfreuden@gmail.com>
2024-08-20 22:57:06 +0200

    Code: Add tests ReadResultsInBoard, ReadResultsInRace, ReadUsedKartsInBoard

--- RaceLogicTests/Services/RaceDataServiceTests.cs
+++ RaceLogicTests/Services/RaceDataServiceTests.cs
@@ -43,7 +43,7 @@ namespace RaceLogicTests.Services
             var result = _raceDataService.ReadLiquesInBoard(countRows);
 
             // Assert
-            Assert.AreEqual(expectedData, result);
+            Assert.That(result, Is.EqualTo(expectedData));
         }
 
         [Test]
@@ -59,7 +59,7 @@ namespace RaceLogicTests.Services
             var result = _raceDataService.ReadNamesInBoard(countRows);
 
             // Assert
-            Assert.AreEqual(expectedData, result);
+            Assert.That(result, Is.EqualTo(expectedData));
         }
 
         [Test]
@@ -98,8 +98,8 @@ namespace RaceLogicTests.Services
             // Arrange
             var data = new List<List<string>>
             {
-                new List<string> { "Data1", "Data2", "Data3" },
-                new List<string> { "Data4", "Data5", "Data6" }
+                new() { "Data1", "Data2", "Data3" },
+                new() { "Data4", "Data5", "Data6" }
             };
             var dataE = new List<string> { "Data1", "Data2", "Data3", null, null, null, null, null, null, null, 
                                             "Data4", "Data5", "Data6", null, null, null, null, null, null, null };
@@ -154,6 +154,128 @@ namespace RaceLogicTests.Services
             // Act & Assert
             var ex = Assert.Throws<InvalidOperationException>(() => _raceDataService.WriteDataInfoInRace(data, nameOfColumn));
             Assert.That(ex.Message, Is.EqualTo("Количество подсписков превышает MaxKarts"));
+        }
+
+        [Test]
+        public void ReadResultsInBoard_ReturnCorrect()
+        {
+            // Arrange
+            var data = new List<string>() { "1", "2", "3" };
+            var expected = new List<List<int>>() { new() { 1, 2, 3 } };
+            var countRows = 3;
+            string nameOfColumn = "ХІТ";
+            _excelWorkerMock.Setup(x => x.ReadDataInColumn(RaceDataService.StartRowByDefault, It.IsAny<int>(), countRows)).Returns(data);
+
+            // Act
+            var result = _raceDataService.ReadResultsInBoard(nameOfColumn, countRows);
+
+            // Assert
+           Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReadResultsInBoard_ReturnCorrect_WhenCountRowsMoreThanDataCount()
+        {
+            // Arrange
+            var data = new List<string>() { "1", "2", "3" };
+            var expected = new List<List<int>>() { new() { 1, 2, 3 } };
+            var countRows = 5;
+            string nameOfColumn = "ХІТ";
+            _excelWorkerMock.Setup(x => x.ReadDataInColumn(RaceDataService.StartRowByDefault, It.IsAny<int>(), countRows)).Returns(data);
+
+            // Act
+            var result = _raceDataService.ReadResultsInBoard(nameOfColumn, countRows);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReadResultsInBoard_ReturnEmptyList_WhenNameOfColumnDoesNotExist()
+        {
+            // Arrange
+            var expected = new List<List<int>>();
+            var countRows = 3;
+            string nameOfColumn = "DoesNotExist";
+
+            // Act
+            var result = _raceDataService.ReadResultsInBoard(nameOfColumn, countRows);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReadResultsInBoard_ShouldThrowException_WhenDataContainsLetters()
+        {
+            // Arrange
+            var data = new List<string>() { "A", "B", "C" };
+            var countRows = 3;
+            string nameOfColumn = "ХІТ";
+            _excelWorkerMock.Setup(x => x.ReadDataInColumn(RaceDataService.StartRowByDefault, It.IsAny<int>(), countRows)).Returns(data);
+
+            // Act & Assert
+            var ex = Assert.Throws<FormatException>(() => _raceDataService.ReadResultsInBoard(nameOfColumn, countRows));
+            Assert.That(ex.Message, Is.EqualTo("Input string was not in a correct format."));
+        }
+
+        [Test]
+        public void ReadResultsInRace_ShouldReturnCorrectData()
+        {
+            // Arrange
+            var data = new List<List<string>> { new() { "1", "2", "3" } };
+            var expected = new List<List<int>> { new() { 1, 2, 3 } };
+            string nameOfColumn = "Очки";
+            _excelWorkerMock.Setup(x => x.ReadDataInColumnsByName(nameOfColumn, It.IsAny<int>())).Returns(data);
+
+            // Act
+            var result = _raceDataService.ReadResultsInRace(nameOfColumn, 3);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReadResultsInRace_ShouldThrowException_WhenDataContainsLetters()
+        {
+            // Arrange
+            var data = new List<List<string>> { new() { "A", "B", "C" } };
+            var countRows = 3;
+            string nameOfColumn = "ХІТ";
+            _excelWorkerMock.Setup(x => x.ReadDataInColumnsByName(nameOfColumn, It.IsAny<int>())).Returns(data);
+
+            // Act & Assert
+            var ex = Assert.Throws<FormatException>(() => _raceDataService.ReadResultsInRace(nameOfColumn, countRows));
+            Assert.That(ex.Message, Is.EqualTo("Input string was not in a correct format."));
+        }
+
+        [Test]
+        public void ReadUsedKartsInBoard_ShouldReturnCorrectData()
+        {
+            // Arrange
+            var data = new List<string> { "1 2 3", "4 5 6" };
+            var expected = new List<List<int>> { new() { 1, 2, 3 }, new() { 4, 5, 6 } };
+            int count = 2;
+            _excelWorkerMock.Setup(x => x.ReadDataInColumn(RaceDataService.StartRowByDefault, It.IsAny<int>(), count)).Returns(data);
+
+            // Act
+            var result = _raceDataService.ReadUsedKartsInBoard(count);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReadUsedKartsInBoard_ShouldThrowException_WhenDataContainsLetters()
+        {
+            // Arrange
+            var data = new List<string>  { "A", "B", "C" };
+            var countRows = 3;
+            string nameOfColumn = "ХІТ";
+            _excelWorkerMock.Setup(x => x.ReadDataInColumn(It.IsAny<int>(), It.IsAny<int>(), countRows)).Returns(data);
+            // Act & Assert
+            var ex = Assert.Throws<FormatException>(() => _raceDataService.ReadUsedKartsInBoard(countRows));
+            Assert.That(ex.Message, Is.EqualTo("Input string was not in a correct format."));
         }
     }
 }
